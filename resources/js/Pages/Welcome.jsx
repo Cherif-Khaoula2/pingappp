0510b1954d99dd8fecc63ff37795f211d41533d4
Khaoula CHERIF <Khaoula.CHERIF@sarpi-dz.com>
2025-10-29 14:07:32 +0100

    plus option

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -45,21 +45,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                 </div>
 
 
-                <div className="grid grid-nogutter surface-0 text-800">
-                    <div className="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
-                        <section>
-                            <span className="block text-6xl font-bold mb-1">Create the dashboard</span>
-                            <div className="text-6xl text-primary font-bold mb-3">your clients deserve to see</div>
-                            <p className="mt-0 mb-4 text-700 line-height-3">Craft a visually stunning and user-centric dashboard that showcases the information your clients deserve with bold, impactful design elements.</p>
-
-                            <Button label="Learn More" type="button" className="mr-3 p-button-raised" />
-                            <Button label="Live Demo" type="button" className="p-button-outlined" />
-                        </section>
-                    </div>
-                    <div className="col-12 md:col-6 overflow-hidden">
-                        <img src="/images/hero/hero-1.png" alt="hero-1" className="md:ml-auto block md:h-full" style={{ clipPath: 'polygon(8% 0, 100% 0%, 100% 100%, 0 100%)' }} />
-                    </div>
-                </div>
+               
 
             </div>
             </LayoutProvider>
