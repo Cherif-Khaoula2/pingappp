535135f3da4a1aa5fb299c868d6efb5908f63eac
Khaoula CHERIF <Khaoula.CHERIF@sarpi-dz.com>
2025-11-05 11:48:39 +0100

    ✨ nom

--- resources/js/Pages/Ad/ManageAddUser.jsx
+++ resources/js/Pages/Ad/ManageAddUser.jsx
@@ -9,7 +9,7 @@ import { Divider } from "primereact/divider";
 import { Dialog } from "primereact/dialog";
 import { Toast } from "primereact/toast";
 import Layout from "@/Layouts/layout/layout.jsx";
-
+import { Head } from '@inertiajs/react';
 const ManageAddUser = () => {
   const toast = React.useRef(null);
   
@@ -319,6 +319,7 @@ const showBackendError = (message) => {
 
   return (
     <Layout>
+        <Head title="Créer un utilisateur AD" />
       <Toast ref={toast} position="top-center" />
       
       <div className="grid">
