71e1f0cbb909671b42f7eb4506b087733983b3f2
khaoula-hamadouche <khaoulahamadouche2020@gmail.com>
2025-11-06 10:14:18 +0100

    first commit

--- resources/js/Pages/Ad/ActivityLogs.jsx
+++ resources/js/Pages/Ad/ActivityLogs.jsx
@@ -84,8 +84,8 @@ export default function ActivityLogs({ logs, stats, filters }) {
             unblock_user: { icon: 'pi-unlock', severity: 'success', label: 'Déblocage' },
             reset_password: { icon: 'pi-refresh', severity: 'warning', label: 'Reset MDP' },
             create_user: { icon: 'pi-user-plus', severity: 'secondary', label: 'Création' },
-            search_user: { icon: 'pi-search', severity: 'info', label: 'Recherche', color: '#06b6d4' },
-            search_user_result: { icon: 'pi-list', severity: 'info', label: 'Résultats', color: '#0ea5e9' },
+            search_user: { icon: 'pi-search', severity: 'info', label: 'Recherche', color: '#d406b2ff' },
+            search_user_result: { icon: 'pi-list', severity: 'info', label: 'Résultats', color: '#317797ff' },
         };
         return configs[action] || { icon: 'pi-question', severity: null, label: action };
     };
